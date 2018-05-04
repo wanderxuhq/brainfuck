812cb221128bed2b60a4983a3ce9a24e1dc02e35
wanderxuhq <wanderxuhq@gmail.com>
2018-05-05 02:08:40 +0800

    修复括号匹配bug

--- brainfuck.js
+++ brainfuck.js
@@ -1,19 +1,9 @@
+function run(){
 var code = document.getElementById("code").value;
 var mem = Array(1024).fill(0);
 var program = code.split('');
 var cur = 0;
-var brackets = [];
-var curbracket = 0;
-for(var i = 0; i < program.length; i++){
-	var sign = program[i];
-		if(sign === '['){
-			brackets[curbracket] = {l:null, r:null};
-			brackets[curbracket++].l = i;
-		} else if (sign ===']'){
-			brackets[--curbracket].r = i;
-		}
-}
-curbracket = 0;
+var bracket = 0;
 for(var i = 0; i < program.length;){
 	var sign = program[i];
 	switch(sign){
@@ -21,9 +11,43 @@ for(var i = 0; i < program.length;){
 		case '<': cur--; i++; break;
 		case '+': mem[cur]++; i++; break;
 		case '-': mem[cur]--; i++; break;
-		case '.': document.write(String.fromCharCode(mem[cur])); i++; break;
+		case '.': document.write(String.fromCharCode(mem[cur])); console.log(mem[cur]); i++; break;
 		case ',': i++; break;
-		case '[': if(mem[cur] === 0) i = brackets[curbracket++].r; else i++; break;
-		case ']': if(mem[cur] > 0) i = brackets[curbracket].l; else {i++; curbracket--;}; break;
+		//case '[': if(mem[cur] === 0) i = brackets[outcurb][incurb].r + 1; else {i++; incurb++; }; break;
+		//case ']': if(mem[cur] > 0) i = brackets[outcurb][incurb - 1].l + 1; else {i++; incurb--; if(incurb ===0 ) outcurb++; }; break;
+		case '[': if(mem[cur] === 0){
+			console.log(i);
+			for(; i < program.length; i++){
+				if(program[i] === '['){
+					bracket++;
+				} else if(program[i] ===']'){
+					bracket--;
+					if(bracket === 0){
+						break;
+					}
+				}
+			}
+		}
+		i++;
+		break;
+		case ']':
+		console.log(i);
+		if(mem[cur] > 0){
+			for(; i >= 0; i--){
+				if(program[i] === '['){
+					bracket++;
+					if(bracket === 0){
+						break;
+					}
+				} else if(program[i] ===']'){
+					bracket--;
+				}
+			}
+		}
+		i++;
+		break;
+		default: i++; break;
 	}
 }
+}
+run();
